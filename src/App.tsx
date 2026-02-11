91500a0b3147aa33055dbdd2eb4f54aa76176eec
javier.rodriguez <javier.rodriguez.sge@mango.com>
2026-02-11 09:31:50 +0100

    Translate app responses and prompts to Spanish, enhancing user experience for Spanish-speaking users.

--- src/App.tsx
+++ src/App.tsx
@@ -13,19 +13,18 @@ export default function Page() {
   const getNoButtonText = () => {
     const phrases = [
       "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
+      "¿Estás segura?",
+      "¿Y si te lo pido muy bien?",
+      "Por favooor",
+      "Ni con un pastelito de chocolate",
+      "POR FAVOR POOKIE",
+      "Pero... :*(",
+      "Me voy a morir",
+      "Ya estoy muerto",
+      "Ok, estás hablando con el fantasma de Javi",
+      "Por favor, cariño",
       ":((((",
-      "PRETTY PLEASE",
+      "¡POR FAVORRR!",
       "Estoy muerto",
       "No :(",
     ];
@@ -38,7 +37,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">Vamoooooos!!! Te quiero pookie!! ;))</div>
         </>
       ) : (
         <>
@@ -46,14 +45,14 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">¿Quieres tener una cita de San Valentín conmigo?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Siiii
             </button>
             <button
               onClick={handleNoClick}
